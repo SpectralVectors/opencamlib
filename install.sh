bbde5a08021d13fff186b5aafc834b5c823d8493
SpectralVectors <robmuir1@gmail.com>
2026-03-05 08:57:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -222,8 +222,9 @@ install_ci_dependencies() {
         if [ "${OCL_MACOS_ARCHITECTURE}" = "arm64" ]; then
             libomp_tar_loc=$(brew fetch --bottle-tag=arm64_sonoma libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo)
         else
-            libomp_tar_loc=$(brew fetch libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo) # --bottle-tag=sonoma
+            libomp_tar_loc=$(brew fetch --bottle-tag=sonoma libomp | grep -i downloaded | grep tar.gz | cut -f2 -d ":" | xargs echo)
         fi
+        echo "${libomp_tar_loc}"
         temp_dir="/tmp"
         cp "${libomp_tar_loc}" "${temp_dir}/libomp.tar.gz"
         mkdir "${temp_dir}/libomp" || true
