f0d8b5afdf726cdfcd71ea2f9b375a2ad683587e
SpectralVectors <robmuir1@gmail.com>
2026-03-05 10:39:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -238,7 +238,7 @@ install_ci_dependencies() {
         #cp  "/opt/homebrew/Cellar/libomp/22.1.0/lib/libomp.dylib" '/tmp/libomp/libomp/fixed/lib/libomp.dylib' # '/tmp/libomp/libomp/fixed'
         ls -a /opt/homebrew/Cellar/libomp/22.1.0/lib
         #ln -s "/opt/homebrew/Cellar/libomp/22.1.0/lib/libomp.dylib" '/tmp/libomp/libomp/fixed/lib' && ls -a /tmp/libomp/libomp/fixed/lib
-        cp "/opt/homebrew/Cellar/libomp/22.1.0/lib/libomp.dylib" '/tmp/libomp/libomp/fixed/lib/libomp.dylib' && ls -a /tmp/libomp/libomp/fixed/lib
+        cp "/opt/homebrew/Cellar/libomp/22.1.0/lib" '/tmp/libomp/libomp/fixed/lib' && ls -a /tmp/libomp/libomp/fixed/lib
         # mv "/opt/homebrew/Cellar/libomp/22.1.0/lib/libomp.dylib" '/tmp/libomp/libomp/fixed/lib' && ls -a /tmp/libomp/libomp/fixed/lib
         # mv "${libomp_prefix}" "${OPENMP_PREFIX_MACOS}"
     fi
